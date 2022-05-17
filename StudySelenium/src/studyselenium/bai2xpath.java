e59c21bf08fc3d22e5cc0f823d4bc974f428338b
Anh Do <anh.do@codeenginestudio.com>
2022-05-17 17:40:32 +0700

    update bai 2

--- StudySelenium/src/studyselenium/bai2xpath.java
+++ StudySelenium/src/studyselenium/bai2xpath.java
@@ -5,7 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class bai2xpath {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception
+	{ 
 		//Setting the driver path
 		System.setProperty("webdriver.chrome.driver", "C://chromedriver_win32//chromedriver.exe");
 				
@@ -21,13 +22,17 @@ public class bai2xpath {
 		//Putting implicit wait
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
-       //Login
+       //Login without inputting the email
+		String email = "anh.do@gmail.com";
+		//String password = "Vitamin@1234";
 		driver.findElement(By.xpath("//input[@id='_com_liferay_login_web_portlet_LoginPortlet_login']")).clear();
-		driver.findElement(By.xpath("//input[@id='_com_liferay_login_web_portlet_LoginPortlet_login']")).sendKeys("anh.do@gmail.com");
-		driver.findElement(By.xpath("//input[@id='_com_liferay_login_web_portlet_LoginPortlet_password']")).sendKeys("Vitamin@1234");
+		driver.findElement(By.xpath("//input[@id='_com_liferay_login_web_portlet_LoginPortlet_login']")).sendKeys(email);
+		//driver.findElement(By.xpath("//input[@id='_com_liferay_login_web_portlet_LoginPortlet_password']")).sendKeys(password);
 		driver.findElement(By.xpath("//input[@id='_com_liferay_login_web_portlet_LoginPortlet_rememberMe']")).click();
 		driver.findElement(By.xpath("//button[@class='btn  btn-primary']")).click();
-		
+		// Print error message when leaving password field blank
+	    System.out.print(driver.findElement(By.xpath("//div[@class='required']")).getText());
+	   
 		
 		//Putting implicit wait
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
